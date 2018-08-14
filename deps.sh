7acc1bbe67aacd11836a8e3a57d1a7aee2fe3495
UVClay <clay@uvclay.com>
2018-08-14 14:09:22 -0400

    updates n stuff

---
+++ deps.sh
@@ -0,0 +1,24 @@
+sudo dnf install git make gcc gcc-c++ autoconf cmake ccache \
+libconfig{,-devel} \
+cairo{,-devel} \
+pango{,-devel} \
+gdk-pixbuf2{,-devel} \
+alsa-lib{,-devel} \
+xcb-util{,-devel} \
+xcb-util-wm{,-devel} \
+wireless-tools{,-devel} \
+libxkbcommon{,-devel} \
+libxcb{,-devel} \
+python2{,-devel} \
+xcb-proto \
+jsoncpp{,-devel} \
+xcb-util-image{,-devel} \
+xcb-util-wm{,-devel} \
+xcb-util-cursor{,-devel} \
+xcb-util-xrm{,-devel} \
+pulseaudio-libs{,-devel} \
+libmpdclient{,-devel} \
+libcurl{,-devel} \
+imlib2{,-devel} \
+libexif{,-devel} \
+giflib{,-devel}
